8003426a25f104f270a1827428d8e39d4d839605
Ryougi-Maker <1101775840@qq.com>
2025-01-06 23:23:20 +0800

    preparing inheritance

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -3,38 +3,162 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : Entity
 {
-    private Rigidbody2D rb;
-    private Animator anim;
 
-
-
+    [Header("Move info")]
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
 
-    [SerializeField] private bool isMoving;
+    [Header("Dash info")]
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private float dashDuration;
+    private float dashTime;
+    [SerializeField] private float dashCoolDown;
+    private float dashCoolDownTimer;
+    [Header("Attack info")]
+    [SerializeField] private float comboTime = .3f;
+    private float comboTimeWindow;
+    private bool isAttacking;
+    private int ComboCounter;
+
+
+
     private float xInput;
-    
-    // Start is called before the first frame update
-    void Start()
+
+    protected override void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        anim = GetComponentInChildren<Animator>();
+        base.Start();
+            
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Update()
+    {
+        base.Update();
+        Movement();
+        CheckInput();
+
+        dashTime -= Time.deltaTime;
+        dashCoolDownTimer -= Time.deltaTime;
+        comboTimeWindow -= Time.deltaTime;
+        
+
+        FlipController();
+        AnimatorControllers();
+
+    }
+
+    public void AttackOver()
+    {
+        isAttacking = false;
+
+        ComboCounter++;
+
+        if (ComboCounter > 2)
+        {
+            ComboCounter = 0;
+        }
+        
+    }
+
+
+    //获取输入
+    private void CheckInput()
     {
         xInput = Input.GetAxisRaw("Horizontal");
-        
-        rb.velocity = new Vector2(xInput*moveSpeed, rb.velocity.y);
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            StartAttackEvent();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jump();
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            DashAbility();
+        }
+    }
+
+    private void StartAttackEvent()
+    {
+        if (!isGrounded)
+        {
+            return;
+        }
+        if (comboTimeWindow < 0)
+        {
+            ComboCounter = 0;
+        }
+
+        isAttacking = true;
+        comboTimeWindow = comboTime;
+    }
+
+    private void DashAbility()
+    {
+        if(dashCoolDownTimer < 0 && !isAttacking)
+        {
+            dashCoolDownTimer = dashCoolDown;
+            dashTime = dashDuration;
+        }
+    }
+
+    //移动模组
+    private void Movement()
+    {
+        if (isAttacking)
+        {
+            rb.velocity = new Vector2(0,0); 
+        }
+        else if (dashTime > 0)
+        {
+            rb.velocity = new Vector2(facingDir * dashSpeed, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+        }
+    }
+
+    //跳跃模组
+    private void jump()
+    {
+        if (isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
 
-        isMoving = rb.velocity.x !=0;
-        anim.SetBool("isMoving",isMoving);
     }
+
+
+    //角色动画切换
+    private void AnimatorControllers()
+    {
+        bool isMoving = rb.velocity.x !=0;
+        anim.SetFloat("yVelocity",rb.velocity.y);
+
+        anim.SetBool("isMoving",isMoving);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetBool("isDashing", dashTime > 0);
+        anim.SetBool("isAttacking", isAttacking);
+        anim.SetInteger("ComboCounter", ComboCounter);
+    }
+
+
+
+    private void FlipController()
+    {
+        if (rb.velocity.x > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (rb.velocity.x<0 && facingRight)
+        {
+            Flip();
+        }
+    }
+
 }
